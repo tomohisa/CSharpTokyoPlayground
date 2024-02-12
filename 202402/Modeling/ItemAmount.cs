237139e6d1050c2a79ecf08bf4324ff792cc2d4a
Tomohisa Takaoka <ttakaoka@jtechs.com>
2024-02-12 10:39:04 -0800

    Add Error Handling

--- 202402/Modeling/ItemAmount.cs
+++ 202402/Modeling/ItemAmount.cs
@@ -1,21 +1,21 @@
 using System.ComponentModel.DataAnnotations;
 namespace _202402.Modeling;
 
-public record JapaneseYen(
-    [property: Range(0, 99999999)]
+public record ItemAmount(
+    [property: Range(1, 100)]
     int Value)
 {
-    public static JapaneseYen Empty => new(0);
-    public static (JapaneseYen, string) Parse(string arg1)
+    public static ItemAmount Empty => new(0);
+    public static (ItemAmount, string) Parse(string arg1)
     {
         var errorMessage = string.Empty;
         if (!int.TryParse(arg1, out var x))
         {
-            errorMessage += "所持金が数値ではありません。";
+            errorMessage += "個数が数値ではありません。";
         }
         var validationResults = new List<ValidationResult>();
-        var yen = new JapaneseYen(x);
-        Validator.TryValidateObject(yen, new ValidationContext(yen), validationResults, true);
+        var amount = new ItemAmount(x);
+        Validator.TryValidateObject(amount, new ValidationContext(amount), validationResults, true);
         if (validationResults.Any())
         {
             errorMessage += string.Join(Environment.NewLine, validationResults.Select(v => v.ErrorMessage));
@@ -24,6 +24,6 @@ public record JapaneseYen(
         {
             return (Empty, errorMessage);
         }
-        return (new JapaneseYen(x), string.Empty);
+        return (amount, string.Empty);
     }
 }
--- 202402/Program.cs
+++ 202402/Program.cs
@@ -1,63 +1,60 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using _202402.Modeling;
-var x = int.Parse(args[0]);
-var y = int.Parse(args[1]);
+var (コマンド, errorMessage) = ラーメン購入コマンド.Parse(args);
 
-Console.WriteLine($"所持金{x}円で、濃厚MAXラーメンを{y}個購入希望ですね。");
+if (!string.IsNullOrEmpty(errorMessage))
+{
+    Console.WriteLine(errorMessage);
+    return;
+}
+Console.WriteLine($"所持金{コマンド.所持金.Value}円で、濃厚MAXラーメンを{コマンド.個数.Value}個購入希望ですね。");
 var カート = AddingOrder.Empty;
-var 所持金 = new JapaneseYen(x);
-var 注文可能 = カート.買えますか(所持金, new OrderDetail(new 濃厚MAXラーメン(), new ItemAmount(y)));
-if (!注文可能)
+if (!カート.買えますか(コマンド.所持金, new OrderDetail(new 濃厚MAXラーメン(), コマンド.個数)))
 {
     Console.WriteLine("申し訳ありませんが、所持金が足りません。");
     return;
 }
-var ラーメン追加済みカート = カート.注文に追加(new OrderDetail(濃厚MAXラーメン.Instance, new ItemAmount(y)));
-if (ラーメン追加済みカート is ErrorOrder error)
+switch (カート.注文に追加(new OrderDetail(濃厚MAXラーメン.Instance, コマンド.個数)))
 {
-    Console.WriteLine(error.Message);
-    return;
+    case ErrorOrder error:
+        Console.WriteLine(error.Message);
+        return;
+    case AddingOrder order:
+        カート = order;
+        Console.WriteLine($"{濃厚MAXラーメン.Instance.Name}を{コマンド.個数.Value}個カートに追加しました。");
+        Console.WriteLine($"合計金額は{カート.TotalPrice.Value:N0}円です。");
+        break;
+    default:
+        throw new InvalidOperationException();
 }
-カート = ラーメン追加済みカート as AddingOrder ?? throw new InvalidOperationException();
-Console.WriteLine($"{濃厚MAXラーメン.Instance.Name}を{y}個カートに追加しました。");
-Console.WriteLine($"合計金額は{カート.TotalPrice.Value:N0}円です。");
-
 Console.WriteLine($"残りの所持金で買えるだけ{叉焼スペシャル炒飯.Instance.Name}を購入希望ですね。");
 var チャーハンの個数 = 0;
-while (true)
+while (カート.買えますか(コマンド.所持金, new OrderDetail(叉焼スペシャル炒飯.Instance, new ItemAmount(チャーハンの個数 + 1))))
 {
-    if (カート.買えますか(所持金, new OrderDetail(叉焼スペシャル炒飯.Instance, new ItemAmount(チャーハンの個数 + 1))))
-    {
-        チャーハンの個数++;
-    }
-    else
-    {
+    チャーハンの個数++;
+}
+switch (チャーハンの個数)
+{
+    case 0:
+        Console.WriteLine("チャーハンが買えません。");
         break;
-    }
-}
-if (チャーハンの個数 == 0)
-{
-    Console.WriteLine("チャーハンが買えません。");
-}
-else
-{
-    var チャーハン追加済みカート = カート.注文に追加(new OrderDetail(叉焼スペシャル炒飯.Instance, new ItemAmount(チャーハンの個数)));
-    if (チャーハン追加済みカート is ErrorOrder error2)
-    {
-        Console.WriteLine(error2.Message);
-        return;
-    }
-    カート = チャーハン追加済みカート as AddingOrder ?? throw new InvalidOperationException();
-    Console.WriteLine($"{叉焼スペシャル炒飯.Instance.Name}を{チャーハンの個数}個カートに追加しました。");
+    default:
+        カート = カート.注文に追加(new OrderDetail(叉焼スペシャル炒飯.Instance, new ItemAmount(チャーハンの個数))) switch
+        {
+            ErrorOrder error => throw new InvalidOperationException(error.Message),
+            AddingOrder order when チャーハンの個数 > 0 => order,
+            _ => throw new InvalidOperationException("Unable to add Fried Rice.")
+        };
+        Console.WriteLine($"{叉焼スペシャル炒飯.Instance.Name}を{チャーハンの個数}個カートに追加しました。");
+        break;
 }
 Console.WriteLine("決済を実行します。");
-var 決済後 = カート.注文を確定する(所持金);
-if (決済後 is ErrorOrder error3)
+var 最終カート = カート.注文を確定する(コマンド.所持金) switch
 {
-    Console.WriteLine(error3.Message);
-    return;
-}
-var 最終カート = 決済後 as ConfirmedOrder ?? throw new InvalidOperationException();
+    ErrorOrder error => throw new InvalidOperationException(error.Message),
+    ConfirmedOrder confirmed => confirmed,
+    _ => throw new InvalidOperationException("Unable to confirm the order.")
+};
 Console.WriteLine("------決済完了------");
 foreach (var item in 最終カート.Items)
 {
